bcfd10b562a8a0042ef68f838ccf49af443b940f
Vipin B <vipin@vipin.xyz>
2025-01-12 14:52:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# qangr
+
+qangr is a streamlined CLI tool (a wrapper for the Angr framework) designed to simplify and accelerate CTF challenge solving.
+
+While Angr is a powerful and versatile framework, it can be overwhelming for beginners or impractical for quick tasks. qangr focuses on the essential features needed to reverse and analyze binaries, offering a simple and efficient CLI experience. It may not include all of Angr’s advanced capabilities, but it’s perfect for quickly pwning simple challenges in seconds.
+
+# Install
+
+Run these commands to quickly add it to your path.
+
+```bash
+git clone https://github.com/qvipin/qangr.git
+cd qangr/
+pip install -r requirements.txt
+mv qangr.py qangr && sudo cp qangr /usr/local/bin
+qangr
+```
+
+# Usage
+
+```txt
+usage: qangr [-h] -g <0x00000> [-b <0x00000>] [-B <0x00000 or auto>] [--DFS] [--binary-info] [--angr-logging-level {DEBUG,INFO}] <binary>
+
+A streamlined CLI tool (a wrapper for the Angr framework) designed to simplify and accelerate CTF challenge solving.
+
+Made with ❤️ by qvipin
+
+positional arguments:
+  <binary>              Target Binary (e.g. chall or crackme)
+
+options:
+  -h, --help            show this help message and exit
+  -g <0x00000>, --good-address <0x00000>
+                        Good Address (Win Func)
+  -b <0x00000>, --bad-address <0x00000>
+                        Bad Address (e.g. puts(str: "Incorrect!"))
+  -B <0x00000 or auto>, --base-address <0x00000 or auto>
+                        Base Address; Use 0x400000 or Specify AUTO for PIE Binaries (Default is `0x000000`)
+  --DFS                 Uses DFS exploration instead of the default BFS Exploration
+  --binary-info         Specify argument for additional binary information.
+  --angr-logging-level {DEBUG,INFO}
+                        Logging level for angr (Default: WARNING and higher). Use --angr-logging-level=<value>
+```
+
+# Examples
+
+See [this]() blog for some qangr examples.
+
+# License
+
+Copyright (C) 2024 qvipin
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
---
+++ qangr.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+import logging
+import argparse
+import sys
+import angr, claripy 
+
+"""Argparse Stuff"""
+
+about = "A streamlined CLI tool (a wrapper for the Angr framework) designed to simplify and accelerate CTF challenge solving.\n\nMade with ❤️ by qvipin"
+parser = argparse.ArgumentParser(description=about, formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("binary", metavar="<binary>", help='Target Binary (e.g. chall or crackme)')
+# will add input through args soon...
+# parser.add_argument("-IA", "--input-through-args", metavar="<Bits>", help="Use if binary takes input through args (e.g., ./chall <password>) and specify max bit size", required=False)
+parser.add_argument("-g", "--good-address", metavar="<0x00000>", help='Good Address (Win Func)', required=True)
+parser.add_argument("-b", "--bad-address", metavar="<0x00000>", help='Bad Address (e.g. puts(str: "Incorrect!"))', required=False)
+parser.add_argument("-B", "--base-address", metavar="<0x00000 or auto>", help='Base Address; Use 0x400000 or Specify AUTO for PIE Binaries (Default is `0x000000`)', required=False)
+parser.add_argument("--DFS", action='store_true', help='Uses DFS exploration instead of the default BFS Exploration', required=False)
+parser.add_argument("--binary-info", action='store_true', help='Specify argument for additional binary information.', required=False)
+parser.add_argument("--angr-logging-level", help='Logging level for angr (Default: WARNING and higher). Use --angr-logging-level=<value>.', choices=["DEBUG", "INFO"],  required=False)
+
+args = parser.parse_args()
+path_to_binary = args.binary # Binary Path Here
+
+from pwn import context, ELF # Import moved here due to ArgParse issues
+
+"""Binary Info"""
+if args.binary_info:
+    context.log_level = 'warning'
+    print(ELF(path_to_binary))
+
+""" WIP
+if not sys.maxsize > 2**32 and binary.arch != ["i386", "amd64"]:
+    print("[*] Your system isn't x86_64, the binary may be incompatible for u")
+"""
+
+
+"""Angr Stuff"""
+try:
+    if args.angr_logging_level: # Managing log levels for angr and pwn
+        logging.getLogger('angr').setLevel(f'{args.angr_logging_level}')
+    logging.getLogger("pwnlib.elf.elf").setLevel(logging.ERROR)
+
+    project = angr.Project(path_to_binary, auto_load_libs=False) 
+
+# base address checks
+    if args.base_address is None:
+        base = 0x000000
+    elif args.base_address.lower() == "auto":
+        base = project.loader.main_object.min_addr # auto base address search
+    else:
+        base = int(args.base_address, 16)
+
+    good_address = base + int(args.good_address, 16)
+
+# entry state/sim
+    initial_state = project.factory.entry_state()
+    sim = project.factory.simgr(initial_state) # Sim starts here.
+    print('[*] Exploring ts (this) 💔...')
+
+# checks for options and the specified way on simulation
+    if args.bad_address and args.DFS is None: # If bad address is specified but no DFS
+        bad_address = base + int(args.bad_address, 16)
+        sim.explore(find=good_address, avoid=bad_address)
+    elif args.bad_address and args.DFS: # bad addr and dfs 
+        bad_address = base + int(args.bad_address, 16)
+        sim.use_technique(angr.exploration_techniques.DFS())
+        sim.explore(find=good_address, avoid=bad_address)
+    else: # nothing
+        sim.explore(find=good_address)
+
+# Checking if Simgr found anything
+    if sim.found: 
+        solution_state = sim.found[0]
+        solution = solution_state.posix.dumps(sys.stdin.fileno())
+        print(f"[*] Solution found: {solution}")
+    elif not args.base_address and ELF(path_to_binary).pie: # Checking if base address is not specified and if PIE is enabled
+         print("[*] Solution not found. PIE detected. Please specify a base address (e.g., 0x400000).")
+    elif not args.bad_address or not args.base_address:
+        print("[*] Solution not found. Providing all optional arguments may improve your chances of success. Please specify them and try again.")
+    else:
+        print("[*] Solution not found.")
+except ValueError:
+    print("[*] Error: Please provide valid arguments. Use -h for help.")
+except KeyboardInterrupt:
+    print("[*] CTRL + C Detected, quitting program.")
